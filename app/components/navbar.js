822e2e98701b3093cb6c65b083d2339741812a88
Niladri Adhikary <rickyrickado14@gmail.com>
2023-10-10 19:18:21 +0530

    landing page added

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -2,17 +2,18 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between px-8 py-4 border-b border-slate-600">
       <div className="flex gap-8 items-center">
-        <h1 className="text-2xl tracking-wide font-bold">Niladri.</h1>
+        <h1 className="text-2xl tracking-wide font-bold text-slate-300">Niladri Adhikary.</h1>
         <ul className="flex gap-8 text-slate-400 text-sm tracking-wide">
           <li>About</li>
           <li>Skills</li>
           <li>Education</li>
           <li>Experience</li>
-          <li>Works</li>
+          <li>Projects</li>
+          <li>Links</li>
         </ul>
       </div>
       <div className="flex items-center">
-        <p className="text-slate-400 text-sm tracking-wide">Contact</p>
+        <p className="text-slate-400 text-sm tracking-wide">Resume</p>
       </div>
     </nav>
   )
