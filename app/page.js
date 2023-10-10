5c879e61d662c406c98c6e0dcc83cb5830359edc
Niladri Adhikary <rickyrickado14@gmail.com>
2023-10-10 13:53:32 +0530

    feat: navbar created

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
-    <main>
+    <main className="flex justify-center h-96 items-center">
+      <h1 className="text-8xl text-slate-400 tracking-tighter font-extrabold">
+        Niladri Sekhar Adhikary...
+      </h1>
     </main>
   )
 }
